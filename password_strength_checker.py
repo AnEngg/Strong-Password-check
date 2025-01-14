f025cdb8209a3b28a836b249af9beb7f5ed8c824
Mauli Bondare <bondaremauli10@gmail.com>
2025-01-14 10:03:26 +0530

    initial commit

---
+++ password_strength_checker.py
@@ -0,0 +1,44 @@
+import re
+
+# Password strength check condition:
+# min 8 chars, digit, uppercase, lowercase, special char
+
+def check_pass_strength(password):
+    # Check if the password length is at least 8 characters
+    if len(password) < 8:
+        return 'Weak: password must be at least 8 character long'
+
+    # Check if password contains at least one digit
+    if not any(char.isdigit() for char in password):
+        return "Weak: password must contain a digit"
+
+    # Check if password contains at least one uppercase letter
+    if not any(char.isupper() for char in password):
+        return "Weak: password must contain an uppercase letter"
+
+    # Check if password contains at least one lowercase letter
+    if not any(char.islower() for char in password):
+        return "Weak: password must contain a lowercase letter"
+    
+    # Check if password contains at least one special character
+    if not re.search(r'[!@#$%^&*()\-+={}[\]|:"<>,./?]', password):
+        return "Medium: password must contain a special character"
+    
+    return "Strong: Your password is secure!"
+
+def password_checker():
+    print("Welcome to the password strength checker")
+
+    while True:
+        password = input("Enter your password (or type 'exit' to quit):")
+        
+        if password.lower() == 'exit':
+            print("Thank you for using this tool!")
+            break
+        
+        result = check_pass_strength(password)
+        print(result)
+
+# Run the password checker tool
+if __name__ == "__main__":
+    password_checker()
